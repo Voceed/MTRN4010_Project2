9a3d870fd6465231ad94264ad9f42ae1a374f726
Voceed <vcozhi@hotmail.com>
2021-02-23 19:41:34 +0800

    made code looks tidy

--- Project2_Files/ShowData2021.m
+++ Project2_Files/ShowData2021.m
@@ -4,66 +4,58 @@
 % file.
 % see "ShowData2021_Example2.m"
 
-
 %Jose Guivant. MTRN4010
-
 
 function Main()
 
-Data=load('Measurements_AAS01.mat');       %We load the data
-Data=Data.A; 
+    Data = load('Measurements_AAS01.mat');       %Load the data in sturt A
+    Data = Data.A; 
 
-    t=Data.t;                                                 % sample times, {t}.     
-    t=double(t)*0.0001;                             %scale time to seconds; original data is integer type, 1 count = 0.1 ms.
+    t = Data.t;              %Sample times, {t}.     
+    t = double(t)*0.0001;    %Scale time to seconds; original data is integer type, 1 count = 0.1 ms.
 
-    %t0=t(1); 
+    %t0 = t(1); 
     
     %Data.Z(1,:): speed from UGV 's encoder. Experessed in mm/sec 
     %Data.Z(2,:): rate provided by GyroZ. Integer, 1 count=0.01 degree/second.  
     
-   % Scale speed to m/s ; originally in mm/s, integer type.
-   % scale ehading rate to degrees/s ; originally in integer type, [1 count = 1/100 degrees/sec].
-    wz=double(Data.Z(2,:))/100;                    % GyroZ readings, "{Wz(t)}"
-    v=double(Data.Z(1,:))/1000;                     % speed readings   "{v(t)}" 
+    %Scale speed to m/s ; originally in mm/s, integer type.
+    %Scale heading rate to deg/s ; originally in integer type, [1 count = 1/100 degrees/sec].
+    
+    wz = double(Data.Z(2,:))/100;      %GyroZ readings, "{Wz(t)}"
+    v = double(Data.Z(1,:))/1000;      %Speed readings   "{v(t)}" 
    
+    %Plot those measurements.
+    figure(1); clf();
+    subplot(211); plot(t,v); xlabel('time (seconds'); ylabel('speed (m/s)');
+    subplot(212); plot(t,wz); xlabel('time (seconds'); ylabel('angular rate (degrees/s)');
+
+    %This is the high intensity index array.
+    ii = find(Data.Z(3,:)>0);   %All the high intensity index.
+    timeL = t(ii);              %Timestamp of the the high intensity index.
+    nL = numel(ii);             %Number of high intensity index.
     
+    %Empty lasers span array. X-axis.
+    angleL = (0:360)/2;
     
-% plot those measurements.
-figure(1) ; clf();
-subplot(211) ; plot(t,v);  xlabel('time (seconds'); ylabel('speed (m/s)');
-subplot(212) ; plot(t,wz); xlabel('time (seconds'); ylabel('angular rate (degrees/s)');
+    %Distances array of each laser.
+    scan = single(Data.scans(:,1))*0.01;    %Scan in metres. Y-axis.
+    %First polar plot of lasers scan.
+    figure(2); clf(); 
+    hL = plot(angleL, scan, '.');   %Plot handle
+    zoom on; grid on; axis([0,180,0,20]);
+    title('showing LiDAR scans');
+    ylabel('range (m)'); xlabel('angle (degrees)');
 
-
-
- ii = find(Data.Z(3,:)>0);
- timeL = t(ii);
- nL = numel(ii);
- 
- 
- angleL = [0:360]/2;
- 
- scan = single(Data.scans(:,1))*0.01;
- figure(2) ; clf(); hL=plot(angleL,scan,'.') ;  zoom on; grid on;
- title('showing LiDAR scans');
- ylabel('range (m)'); xlabel('angle (degrees)');
- axis([0,180,0,20]);
- 
- % show all the scans
- 
- 
- % show LiDAR scans (animation)
- 
- u0=200;
- for u=u0:nL
- i = ii(u);
-     scan = single(Data.scans(:,i))*0.01;
-    set(hL,'ydata',scan);
-     pause(0.1);
- end;
-  
- 
-end 
-
-% -------------------------------------------------------
+    %Continously update the plot with newer scan.
+    u0 = 200;
+    for u = u0:nL
+        i = ii(u);
+    	scan = single(Data.scans(:,i))*0.01;
+        set(hL,'ydata',scan);
+        pause(0.1);
+    end
+    
+end
 
 
